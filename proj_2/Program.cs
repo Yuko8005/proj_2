36aeb0ff2f30470db2efec92e5cf1be95cbfee20
Yuko8005 <yukoko6789@gmail.com>
2023-06-09 13:10:29 +0300

    first commit proj_2

---
+++ proj_2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.Win32;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var subKeyNames = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
+
+        Console.WriteLine(subKeyNames);
+        string[] valueNames = subKeyNames.GetValueNames();
+        foreach (var item in valueNames)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.ReadLine();
+    }
+}
